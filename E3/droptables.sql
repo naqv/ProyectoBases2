beced6b1a13a1b11476ac6469e881bef1717ac2e
naqv <naqv1991@gmail.com>
2013-12-17 01:17:47 -0430

    FUNCIONA AL PELO EL DROPTABLE xD

--- E3/droptables.sql
+++ E3/droptables.sql
@@ -1,32 +1,48 @@
-DROP TABLE usuario_table;
-DROP TABLE hito_table;
-DROP TABLE ruta_table;
-DROP TABLE coordenada_table;
-DROP TABLE telefonos_tab;
-DROP TABLE ciudad_table;
+
+DROP TABLE paquete_table;
+DROP TABLE valoracion_table;
+DROP TABLE evento_table;
 DROP TABLE boolean_table;
 DROP TABLE nombres_table;
-DROP TABLE evento_table;
-DROP TABLE via_table;
-DROP TABLE servicio_table;
-DROP TABLE valoracion_table;
-DROP TABLE paquete_table;
+DROP TABLE coordenada_table;
+DROP TABLE telefonos_tab;
+DROP TABLE via_table CASCADE CONSTRAINTS;
+DROP TABLE servicio_table CASCADE CONSTRAINTS;
+DROP TABLE ciudad_table CASCADE CONSTRAINTS;
+DROP TABLE ruta_table CASCADE CONSTRAINTS;
+DROP TABLE hito_table CASCADE CONSTRAINTS;
+DROP TABLE usuario_table CASCADE CONSTRAINTS;
+
 -- TYPES
+
+DROP TYPE paquete_t;
+DROP TYPE valoracion_t;
+DROP TYPE evento_t;
+DROP TYPE fija_t;
+DROp TYPE dinamica_t;
+DROP TYPE nombres_t FORCE;
+DROP TYPE telefono_t FORCE;
+DROP TYPE coordenada_t FORCE;
+DROP TYPE servicio_t;
+DROP TYPE via_t;
+DROP TYPE ruta_t;
+DROP TYPE usuario_t;
+DROP TYPE ciudad_t;
+DROP TYPE hito_t;
 DROP TYPE multis_t;
 DROP TYPE multi_t;
-DROP TYPE usuario_t;
-DROp TYPE dinamica_t;
-DROP TYPE fija_t;
-DROP TYPE ruta_t;
-DROP TYPE hito_t;
-DROP TYPE coordenada_t;
-DROP TYPE boolean_t;
-DROP TYPE ciudad_t;
-DROP TYPE telefono_t;
-DROP TYPE nombres_t;
-DROP TYPE via_t;
-DROP TYPE evento_t;
-DROP TYPE valoracion_t;
-DROP TYPE servicio_t;
-DROP TYPE paquete_t;
+DROP TYPE boolean_t FORCE;
+
+
+
+
+
+
+
+
+
+
+
+
+
 /
