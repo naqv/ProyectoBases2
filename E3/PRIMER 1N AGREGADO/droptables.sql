13f5f9cc5ac870718af40454dd0d9a793ffb3a89
naqv <naqv1991@gmail.com>
2013-12-18 02:21:24 -0430

    ACT.

---
+++ E3/PRIMER 1N AGREGADO/droptables.sql
@@ -0,0 +1,56 @@
+
+DROP TABLE servicio_table;
+DROP TABLE nombres_table;
+DROP TABLE telefonos_tab;
+DROP TABLE boolean_table;
+DROP TABLE coordenada_table;
+DROP TABLE valoraciudad_table;
+DROP TABLE valorahito_table;
+DROP TABLE valoraruta_table;
+DROP TABLE contrata_table;
+DROP TABLE evento_table;
+DROP TABLE Toma_table;
+DROP TABLE via_table;
+DROP TABLE usuario_table;
+drop table valoracion_table;
+DROP TABLE paquete_table;
+DROP TABLE ruta_table;
+DROP TABLE ciudad_table;
+DROP TABLE hito_table;
+-- TYPES
+
+
+DROP TYPE valoraciudad_t;
+DROP TYPE Toma;
+DROP TYPE evento_t;
+DROP TYPE fija_t;
+DROP TYPE dinamica_t;
+DROP TYPE valorahito_t;
+DROP TYPE valoraruta_t;
+DROP TYPE SeAccedePor;
+DROP TYPE TieneServicio;
+DROP TYPE Tiene;
+DROP TYPE time_t;
+DROP TYPE valoracion_t FORCE;
+DROP TYPE ciudad_t FORCE;
+DROP TYPE ref_to_via;
+DROP TYPE refs_to_via;
+DROP TYPE via_t;
+DROP TYPE telefono_t FORCE;
+DROP TYPE coordenada_t FORCE;
+DROP TYPE nombres_t FORCE;
+DROP TYPE boolean_t FORCE;
+DROP TYPE usuario_t;
+DROP TYPE hito_t;
+DROP TYPE ruta_t;
+-- DROP TYPE ciudad_t;
+DROP TYPE servicio_t;
+DROP TYPE paquete_t;
+DROP TYPE multis_t FORCE;
+DROP TYPE multi_t FORCE;
+DROP TYPE acompanante_t FORCE;
+DROP TYPE acompanantes_t FORCE;
+DROP TYPE agr_via_t;
+DROP TYPE agr_hito_t;
+
+/
