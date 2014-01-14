746d8ed29d4795e0f8978ce7c50d004401cb5535
yeiker_7 <yeikervazmol@gmail.com>
2014-01-14 14:02:31 -0430

    Se depura la entrega 3 y quedan solo los archivos que funcionan.

--- E3/COSASDENESTEDTABLES/EJEMPLO PARA NATACHA/tablas.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE empleado_table OF empleado_t;
-
-CREATE TABLE departamento_table OF departamento_t
-    NESTED TABLE empleadosActuales STORE AS EMPLEADOS_ACTUALES;
--- E3/droptables.sql
+++ E3/droptables.sql
@@ -1,7 +1,4 @@
-
-
-
- drop table contrata_table;
+drop table contrata_table;
  drop table seaccedepor_table;
  drop table tiene_table;
  drop table tieneservicio_table;
@@ -31,7 +28,6 @@ drop table ciudad_table;
 -- TYPES
 DROP TYPE valoraciudad_t;
 DROP TYPE Toma;
-DROP TYPE evento_t;
 DROP TYPE fija_t;
 DROP TYPE dinamica_t;
 DROP TYPE valorahito_t;
@@ -40,21 +36,25 @@ DROP TYPE SeAccedePor;
 DROP TYPE TieneServicio;
 DROP TYPE Tiene;
 DROP TYPE time_t;
-DROP TYPE valoracion_t FORCE;
-DROP TYPE via_t FORCE;
+DROP TYPE agr_via_t;
+DROP TYPE agr_hito_t;
+DROP TYPE valoracion_t ;
 DROP TYPE telefono_t FORCE;
 DROP TYPE coordenada_t FORCE;
 DROP TYPE nombres_t FORCE;
 DROP TYPE boolean_t FORCE;
+DROP TYPE multis_t FORCE;
+DROP TYPE multi_t FORCE;
 DROP TYPE usuario_t;
 DROP TYPE hito_t;
 DROP TYPE ruta_t;
-DROP TYPE ciudad_t FORCE;
+DROP TYPE evento_t;
 DROP TYPE servicio_t;
 DROP TYPE paquete_t;
-DROP TYPE multis_t FORCE;
-DROP TYPE multi_t FORCE;
+DROP TYPE via_t;
+DROP TYPE ciudad_t;
 DROP TYPE acompanante_t FORCE;
-DROP TYPE acompanantes_t FORCE;
-DROP TYPE agr_via_t;
-DROP TYPE agr_hito_t;
+DROP TYPE acompanantes_t ;
+drop type refs_to_ruta_table;
+drop type refs_to_evento;
+drop type refs_to_via;
