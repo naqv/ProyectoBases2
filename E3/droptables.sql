7ec0f0116b01760b33a933e864521ec4bfa6248d
naqv <naqv1991@gmail.com>
2014-01-15 11:04:04 -0430

    agregando los nuevos scripts que estan en mi cuenta del LDC

--- E3/droptables.sql
+++ E3/droptables.sql
@@ -39,10 +39,7 @@ DROP TYPE time_t;
 DROP TYPE agr_via_t;
 DROP TYPE agr_hito_t;
 DROP TYPE valoracion_t ;
-
-
-
-
+purge recyclebin;
 DROP TYPE telefono_t FORCE;
 DROP TYPE coordenada_t FORCE;
 DROP TYPE nombres_t FORCE;
