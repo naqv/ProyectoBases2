ed5083aaca0b1b7ae189b7c68e46051663aeff7c
yeiker_7 <yeikervazmol@gmail.com>
2014-01-23 23:36:02 -0430

    Se eliminan scripts externos al proyecto.

--- E4/Scripts Sept-Dic 2012/CA_FEQ.sql
+++
@@ -1,8 +0,0 @@
-create or replace
-FUNCTION CA_FEQ (N1 IN NUMBER, Dom IN VARCHAR2, Lab IN VARCHAR2) RETURN REAL IS
-  T1 Trapezoid_objtyp;
-  BEGIN
-    T1:=CA_Trap(Dom,Lab);
-    RETURN T1.FEQT(trapezoid_objtyp(N1, N1, N1, N1));
-  END;
-/
